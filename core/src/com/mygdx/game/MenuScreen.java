a26e790e6ff2e2f456d265afbce72d5ff02393c2
Spencer Goles <sgconlisk@gmail.com>
2023-11-12 18:23:45 -0800

    titlescreen

--- core/src/com/mygdx/game/MenuScreen.java
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -21,9 +21,6 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class MenuScreen implements Screen {
     private final SpriteBatch batch;
     private final Music menuMusic;
-    private final TextButton playButton;
-    private final TextButton howToPlayButton;
-    private final TextButton settingsButton;
     private final Stage stage;
 
     public MenuScreen(final SpriteBatch batch, final Music menuMusic) {
@@ -34,53 +31,8 @@ public class MenuScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        // Initialize buttons
-        float buttonWidth = 200;
-        float buttonHeight = 50;
-        float buttonSpacing = 20;
-        float centerX = Gdx.graphics.getWidth() / 2f;
-        float centerY = Gdx.graphics.getHeight() / 2f - 100;
-
-        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
-        buttonStyle.font = new BitmapFont();
-        buttonStyle.font.setColor(Color.BLACK);
-
-        playButton = new TextButton("Play Game", buttonStyle);
-        playButton.setSize(buttonWidth, buttonHeight);
-        playButton.setPosition(centerX - buttonWidth / 2, centerY);
-        playButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                menuMusic.stop(); // Stop current music
-                DinosaurChess.game.setScreen(new PlayGameScreen(batch));
-            }
-        });
 
 
-        howToPlayButton = new TextButton("How to Play", buttonStyle);
-        howToPlayButton.setSize(buttonWidth, buttonHeight);
-        howToPlayButton.setPosition(centerX - buttonWidth / 2, centerY - buttonHeight - buttonSpacing);
-        howToPlayButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                DinosaurChess.game.setScreen(new HowToPlayScreen(batch, menuMusic));
-            }
-        });
-
-        settingsButton = new TextButton("Settings", buttonStyle);
-        settingsButton.setSize(buttonWidth, buttonHeight);
-        settingsButton.setPosition(centerX - buttonWidth / 2, centerY - 2 * (buttonHeight + buttonSpacing));
-        settingsButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                DinosaurChess.game.setScreen(new SettingsScreen(batch, menuMusic));
-            }
-        });
-
-        // Add buttons to the stage
-        stage.addActor(playButton);
-        stage.addActor(howToPlayButton);
-        stage.addActor(settingsButton);
     }
 
     @Override
@@ -108,9 +60,7 @@ public class MenuScreen implements Screen {
 
         batch.end();
 
-        // Draw the stage (buttons)
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-        stage.draw();
+
     }
 
     @Override
